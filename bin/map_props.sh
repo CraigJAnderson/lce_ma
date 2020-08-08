68d2268b75980335807aaf2101a01533ecf17a47
Craig Anderson <caearthworms@gmail.com>
2020-08-08 12:04:27 +0100

    Update map_props.sh

--- bin/map_props.sh
+++ bin/map_props.sh
@@ -1,7 +1,6 @@
 #/!/bin/sh
 ##/hps/nobackup2/flicek/user/cander21/LCE20190618/bin/map_props.sh
 ##this collection of scripts will count up the number of MA and BA sites in the original set of strelka variants. It will also output 100 iterations of the same number of sites from across DEN-sensitive control sites, outputing the number of MA and BA sites. The average proportion of control sites with ALT alleles, considering both BA and MA sites, versus those without any, is reported for each iteration.
-##example use: map_props.sh /hps/nobackup2/flicek/user/cander21/LCE20190618/MA/mpileup/${STRAIN}/ 90791_N2 /hps/nobackup2/flicek/user/cander21/LCE20190618/REF/${STRAIN}.20180918.familialFilter.bed /hps/nobackup2/flicek/user/cander21/LCE20190618/MA/controls/${STRAIN}_dysplastic_den.arc_filtered_control_sites.bed.gz
 NOD=$1
 STRAIN=$2
 ALLSITES=$3
